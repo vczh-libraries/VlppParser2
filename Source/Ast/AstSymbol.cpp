823d04358941a21ecb206a33e531c5816d8388d3
vczh <vczh@outlook.com>
2021-11-08 01:49:42 -0800

    ...

--- Source/Ast/AstSymbol.cpp
+++ Source/Ast/AstSymbol.cpp
@@ -4,5 +4,108 @@ namespace vl
 {
 	namespace glr
 	{
+		namespace parsergen
+		{
+			using namespace collections;
+
+/***********************************************************************
+AstDefFile
+***********************************************************************/
+
+			template<typename T>
+			T* AstDefFile::CreateSymbol(const WString& symbolName)
+			{
+				auto symbol = new T();
+				static_cast<AstSymbol*>(symbol)->ownerFile = this;
+				static_cast<AstSymbol*>(symbol)->name = symbolName;
+				symbols.Add(symbol);
+
+				if (!symbolMap.Keys().Contains(symbolName))
+				{
+					symbolMap.Add(symbolName, symbol);
+				}
+				else
+				{
+					ownerManager->AddError(
+						AstErrorType::DuplicatedSymbol,
+						name,
+						symbolName
+						);
+				}
+
+				if (!ownerManager->symbolMap.Keys().Contains(symbolName))
+				{
+					ownerManager->symbolMap.Add(symbolName, symbol);
+				}
+				else
+				{
+					ownerManager->AddError(
+						AstErrorType::DuplicatedSymbolGlobally,
+						name,
+						symbolName,
+						ownerManager->symbolMap[symbolName]->ownerFile->name
+						);
+				}
+
+				return symbol;
+			}
+
+			AstDefFile::AstDefFile(AstSymbolManager* _ownerManager, const WString& _name)
+				:ownerManager(_ownerManager)
+				, name(_name)
+			{
+			}
+
+			AstEnumSymbol* AstDefFile::CreateEnum(const WString& symbolName)
+			{
+				return CreateSymbol<AstEnumSymbol>(symbolName);
+			}
+
+			AstClassSymbol* AstDefFile::CreateClass(const WString& symbolName)
+			{
+				return CreateSymbol<AstClassSymbol>(symbolName);
+			}
+
+/***********************************************************************
+AstSymbolManager
+***********************************************************************/
+
+			template<typename ...TArgs>
+			void AstSymbolManager::AddError(AstErrorType type, TArgs&& ...args)
+			{
+				AstError error;
+				error.type = type;
+
+				WString sargs[] = { WString(args)... };
+				WString* dargs[] = { &error.arg1,&error.arg2,&error.arg3 };
+				constexpr vint sl = sizeof(sargs) / sizeof(*sargs);
+				constexpr vint dl = sizeof(dargs) / sizeof(*dargs);
+				constexpr vint ml = sl < dl ? sl : dl;
+				for (vint i = 0; i < ml; i++)
+				{
+					*dargs[i] = sargs[i];
+				}
+
+				errors.Add(std::move(error));
+			}
+
+			AstDefFile* AstSymbolManager::CreateFile(const WString& name)
+			{
+				auto file = new AstDefFile(this, name);
+				files.Add(file);
+				if (!fileMap.Keys().Contains(name))
+				{
+					fileMap.Add(name, file);
+				}
+				else
+				{
+					AddError(
+						AstErrorType::DuplicatedFile,
+						name
+						);
+				}
+				return file;
+			}
+		}
 	}
 }
--- Source/Ast/AstSymbol.h
+++ Source/Ast/AstSymbol.h
@@ -6,10 +6,153 @@ Licensed under https://github.com/vczh-libraries/License
 #ifndef VCZH_PARSER2_AST_ASTSYMBOL
 #define VCZH_PARSER2_AST_ASTSYMBOL
 
+#include <Vlpp.h>
+
 namespace vl
 {
 	namespace glr
 	{
+		namespace parsergen
+		{
+			class AstClassSymbol;
+			class AstDefFile;
+			class AstSymbolManager;
+
+/***********************************************************************
+AstSymbol
+***********************************************************************/
+
+			class AstSymbol : public Object
+			{
+				friend class AstDefFile;
+			protected:
+				AstDefFile*					ownerFile = nullptr;
+				WString						name;
+
+			public:
+			};
+
+			class AstEnumItemSymbol : public AstSymbol
+			{
+			protected:
+				vint						value = 0;
+
+			public:
+			};
+
+			class AstEnumSymbol : public AstSymbol
+			{
+				using ItemList = collections::List<Ptr<AstEnumItemSymbol>>;
+				using ItemMap = collections::Dictionary<WString, AstEnumItemSymbol*>;
+			protected:
+				ItemList					items;
+				ItemMap						itemMap;
+
+			public:
+				const ItemMap&				Items() { return itemMap; }
+			};
+
+			enum class AstPropType
+			{
+				Token,
+				Class,
+				ClassArray,
+			};
+
+			class AstClassPropSymbol : public AstSymbol
+			{
+			protected:
+				AstPropType					propType = AstPropType::Token;
+				AstClassSymbol*				classSymbol = nullptr;
+
+			public:
+			};
+
+			class AstClassSymbol : public AstSymbol
+			{
+				using PropList = collections::List<Ptr<AstClassPropSymbol>>;
+				using PropMap = collections::Dictionary<WString, AstClassPropSymbol*>;
+			protected:
+				PropList					props;
+				PropMap						propMap;
+
+			public:
+				const PropMap&				Props() { return propMap; }
+			};
+
+/***********************************************************************
+AstDefFile
+***********************************************************************/
+
+			class AstDefFile : public Object
+			{
+				friend class AstSymbolManager;
+
+				using DependenciesList = collections::List<WString>;
+				using NamespaceItems = collections::List<WString>;
+				using SymbolList = collections::List<Ptr<AstSymbol>>;
+				using SymbolMap = collections::Dictionary<WString, AstSymbol*>;
+			protected:
+				AstSymbolManager*			ownerManager = nullptr;
+				SymbolList					symbols;
+				SymbolMap					symbolMap;
+
+				template<typename T>
+				T*							CreateSymbol(const WString& symbolName);
+
+				AstDefFile(AstSymbolManager* _ownerManager, const WString& _name);
+			public:
+				WString						name;
+				DependenciesList			dependencies;
+				NamespaceItems				nss;
+
+				AstEnumSymbol*				CreateEnum(const WString& symbolName);
+				AstClassSymbol*				CreateClass(const WString& symbolName);
+				const SymbolMap&			Symbols() { return symbolMap; }
+			};
+
+/***********************************************************************
+AstSymbolManager
+***********************************************************************/
+
+			enum class AstErrorType
+			{
+				DuplicatedFile,				// (fileName)
+				DuplicatedSymbol,			// (fileName, symbolName)
+				DuplicatedSymbolGlobally,	// (fileName, symbolName, anotherFileName)
+			};
+
+			struct AstError
+			{
+				AstErrorType				type;
+				WString						arg1;
+				WString						arg2;
+				WString						arg3;
+			};
+
+			class AstSymbolManager : public Object
+			{
+				using FileList = collections::List<Ptr<AstDefFile>>;
+				using ErrorList = collections::List<AstError>;
+				using FileMap = collections::Dictionary<WString, AstDefFile*>;
+				using SymbolMap = collections::Dictionary<WString, AstSymbol*>;
+
+				friend class AstDefFile;
+			protected:
+				FileList					files;
+				ErrorList					errors;
+				FileMap						fileMap;
+				SymbolMap					symbolMap;
+
+			public:
+				template<typename ...TArgs>
+				void						AddError(AstErrorType type, TArgs&& ...args);
+
+				AstDefFile*					CreateFile(const WString& name);
+				const FileMap&				Files() { return fileMap; }
+				const SymbolMap&			Symbols() { return symbolMap; }
+			};
+		}
 	}
 }
 
