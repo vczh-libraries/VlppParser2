59cd84dfe39a6f380f2276d1595d3a64178aea1d
vczh <vczh@outlook.com>
2021-11-14 01:38:15 -0800

    TestAstAssembler.cpp

--- Test/UnitTest/ParserTest_AstParserGen/TestAstAssembler.cpp
+++ Test/UnitTest/ParserTest_AstParserGen/TestAstAssembler.cpp
@@ -4,6 +4,7 @@
 
 using namespace vl;
 using namespace vl::collections;
+using namespace vl::stream;
 using namespace vl::regex;
 using namespace vl::glr;
 using namespace vl::glr::parsergen;
@@ -11,6 +12,42 @@ using namespace calculator;
 
 extern WString GetExePath();
 extern void GenerateCalculatorLexer(LexerSymbolManager& manager);
+
+namespace TestAstAssembler_TestObjects
+{
+	template<typename T>
+	void AssertAst(Ptr<T> ast, const wchar_t* output)
+	{
+		List<WString> expected, actual;
+		{
+			StringReader reader(output);
+			while (!reader.IsEnd())
+			{
+				expected.Add(reader.ReadLine());
+			}
+		}
+		{
+			MemoryStream actualStream;
+			{
+				StreamWriter writer(actualStream);
+				json_visitor::AstVisitor visitor(writer);
+				visitor.printAstCodeRange = false;
+				visitor.printTokenCodeRange = false;
+				visitor.Print(ast.Obj());
+			}
+			actualStream.SeekFromBegin(0);
+			{
+				StreamReader reader(actualStream);
+				while (!reader.IsEnd())
+				{
+					expected.Add(reader.ReadLine());
+				}
+			}
+		}
+		TEST_ASSERT(CompareEnumerable(expected, actual) == 0);
+	}
+}
+using namespace TestAstAssembler_TestObjects;
 
 TEST_FILE
 {
@@ -45,6 +82,13 @@ export 1
 		auto node = receiver.Finished();
 		auto ast = node.Cast<Module>();
 		TEST_ASSERT(ast);
+		AssertAst(ast, LR"({
+    "$ast": "Module",
+    "exported": {
+        "$ast": "NumExpr",
+        "value": "1"
+    }
+})");
 	});
 
 #undef LEXER
