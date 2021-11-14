20a6273e96d8f9d3c281dcb867ee3e33389e8972
vczh <vczh@outlook.com>
2021-11-14 02:15:01 -0800

    ...

--- Test/UnitTest/ParserTest_AstParserGen/TestAstAssembler.cpp
+++ Test/UnitTest/ParserTest_AstParserGen/TestAstAssembler.cpp
@@ -147,5 +147,117 @@ export 1 + 2
 })");
 	});
 
+	TEST_CASE(L"import ... export ...")
+	{
+		WString input = LR"(
+import max
+import sum
+export sum(1, 2, max(3, 4))
+)";
+		LEXER(input, tokens);
+		TEST_ASSERT(tokens.Count() == 18);
+		CalculatorAstInsReceiver receiver;
+		receiver.Execute({ AstInsType::BeginObject, (vint32_t)CalculatorClasses::Module }, tokens[0]);
+
+			receiver.Execute({ AstInsType::BeginObject, (vint32_t)CalculatorClasses::Import }, tokens[1]);
+			receiver.Execute({ AstInsType::Token }, tokens[1]);
+			receiver.Execute({ AstInsType::Field, (vint32_t)CalculatorFields::Import_name }, tokens[1]);
+			receiver.Execute({ AstInsType::EndObject }, tokens[1]);
+			receiver.Execute({ AstInsType::Field, (vint32_t)CalculatorFields::Module_imports }, tokens[1]);
+
+			receiver.Execute({ AstInsType::BeginObject, (vint32_t)CalculatorClasses::Import }, tokens[3]);
+			receiver.Execute({ AstInsType::Token }, tokens[3]);
+			receiver.Execute({ AstInsType::Field, (vint32_t)CalculatorFields::Import_name }, tokens[3]);
+			receiver.Execute({ AstInsType::EndObject }, tokens[3]);
+			receiver.Execute({ AstInsType::Field, (vint32_t)CalculatorFields::Module_imports }, tokens[3]);
+
+			receiver.Execute({ AstInsType::BeginObject, (vint32_t)CalculatorClasses::Call }, tokens[5]);
+
+				receiver.Execute({ AstInsType::BeginObject, (vint32_t)CalculatorClasses::Ref }, tokens[5]);
+				receiver.Execute({ AstInsType::Token }, tokens[5]);
+				receiver.Execute({ AstInsType::Field, (vint32_t)CalculatorFields::Ref_name }, tokens[5]);
+				receiver.Execute({ AstInsType::EndObject }, tokens[5]);
+				receiver.Execute({ AstInsType::Field, (vint32_t)CalculatorFields::Call_func }, tokens[5]);
+
+				receiver.Execute({ AstInsType::BeginObject, (vint32_t)CalculatorClasses::NumExpr }, tokens[7]);
+				receiver.Execute({ AstInsType::Token }, tokens[7]);
+				receiver.Execute({ AstInsType::Field, (vint32_t)CalculatorFields::NumExpr_value }, tokens[7]);
+				receiver.Execute({ AstInsType::EndObject }, tokens[7]);
+				receiver.Execute({ AstInsType::Field, (vint32_t)CalculatorFields::Call_arg }, tokens[7]);
+
+				receiver.Execute({ AstInsType::BeginObject, (vint32_t)CalculatorClasses::NumExpr }, tokens[9]);
+				receiver.Execute({ AstInsType::Token }, tokens[9]);
+				receiver.Execute({ AstInsType::Field, (vint32_t)CalculatorFields::NumExpr_value }, tokens[9]);
+				receiver.Execute({ AstInsType::EndObject }, tokens[9]);
+				receiver.Execute({ AstInsType::Field, (vint32_t)CalculatorFields::Call_arg }, tokens[9]);
+
+				receiver.Execute({ AstInsType::BeginObject, (vint32_t)CalculatorClasses::Call }, tokens[11]);
+
+					receiver.Execute({ AstInsType::BeginObject, (vint32_t)CalculatorClasses::Ref }, tokens[11]);
+					receiver.Execute({ AstInsType::Token }, tokens[11]);
+					receiver.Execute({ AstInsType::Field, (vint32_t)CalculatorFields::Ref_name }, tokens[11]);
+					receiver.Execute({ AstInsType::EndObject }, tokens[11]);
+					receiver.Execute({ AstInsType::Field, (vint32_t)CalculatorFields::Call_func }, tokens[11]);
+
+					receiver.Execute({ AstInsType::BeginObject, (vint32_t)CalculatorClasses::NumExpr }, tokens[13]);
+					receiver.Execute({ AstInsType::Token }, tokens[13]);
+					receiver.Execute({ AstInsType::Field, (vint32_t)CalculatorFields::NumExpr_value }, tokens[13]);
+					receiver.Execute({ AstInsType::EndObject }, tokens[13]);
+					receiver.Execute({ AstInsType::Field, (vint32_t)CalculatorFields::Call_arg }, tokens[13]);
+
+					receiver.Execute({ AstInsType::BeginObject, (vint32_t)CalculatorClasses::NumExpr }, tokens[15]);
+					receiver.Execute({ AstInsType::Token }, tokens[15]);
+					receiver.Execute({ AstInsType::Field, (vint32_t)CalculatorFields::NumExpr_value }, tokens[15]);
+					receiver.Execute({ AstInsType::EndObject }, tokens[15]);
+					receiver.Execute({ AstInsType::Field, (vint32_t)CalculatorFields::Call_arg }, tokens[15]);
+
+				receiver.Execute({ AstInsType::EndObject }, tokens[1]);
+
+			receiver.Execute({ AstInsType::EndObject }, tokens[1]);
+
+		receiver.Execute({ AstInsType::EndObject }, tokens[1]);
+
+		auto node = receiver.Finished();
+		auto ast = node.Cast<Module>();
+		TEST_ASSERT(ast);
+		AssertAst(ast, LR"({
+    "$ast": "Module",
+    "exported": {
+        "$ast": "Call",
+        "arg": [{
+            "$ast": "NumExpr",
+            "value": "1"
+        }, {
+            "$ast": "NumExpr",
+            "value": "2"
+        }, {
+        "$ast": "Call",
+            "arg": [{
+                "$ast": "NumExpr",
+                "value": "3"
+            }, {
+                "$ast": "NumExpr",
+                "value": "4"
+            }],
+            "func": {
+                "$ast": "Ref",
+                "name": "max"
+            }
+        }],
+        "func": {
+            "$ast": "Ref",
+            "name": "sum"
+        }
+    },
+    "imports": [{
+        "$ast": "Import",
+        "name": "max"
+    }, {
+        "$ast": "Import",
+        "name": "sum"
+    }]
+})");
+	});
+
 #undef LEXER
 }
