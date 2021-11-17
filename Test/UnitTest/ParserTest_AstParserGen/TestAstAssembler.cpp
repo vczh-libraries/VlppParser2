5a332f9e2375514aa5d80b0f0a4002b0365918e8
vczh <vczh@outlook.com>
2021-11-17 13:57:23 -0800

    ...

--- Test/UnitTest/ParserTest_AstParserGen/TestAstAssembler.cpp
+++ Test/UnitTest/ParserTest_AstParserGen/TestAstAssembler.cpp
@@ -209,6 +209,53 @@ export 1 + 2
 })");
 	});
 
+	TEST_CASE(L"export 1 + 2 (left recursively)")
+	{
+		WString input = LR"(
+export 1 + 2
+)";
+		LEXER(input, tokens);
+		TEST_ASSERT(tokens.Count() == 4);
+		CalculatorAstInsReceiver receiver;
+		receiver.Execute({ AstInsType::BeginObject, (vint32_t)CalculatorClasses::Module }, tokens[0]);
+		receiver.Execute({ AstInsType::BeginObject, (vint32_t)CalculatorClasses::NumExpr }, tokens[1]);
+		receiver.Execute({ AstInsType::Token }, tokens[1]);
+		receiver.Execute({ AstInsType::Field, (vint32_t)CalculatorFields::NumExpr_value }, tokens[1]);
+		receiver.Execute({ AstInsType::EndObject }, tokens[1]);
+		receiver.Execute({ AstInsType::BeginObjectLeftRecursive, (vint32_t)CalculatorClasses::Binary }, tokens[1]);
+		receiver.Execute({ AstInsType::Field, (vint32_t)CalculatorFields::Binary_left }, tokens[1]);
+		receiver.Execute({ AstInsType::BeginObject, (vint32_t)CalculatorClasses::NumExpr }, tokens[3]);
+		receiver.Execute({ AstInsType::Token }, tokens[3]);
+		receiver.Execute({ AstInsType::Field, (vint32_t)CalculatorFields::NumExpr_value }, tokens[3]);
+		receiver.Execute({ AstInsType::EndObject }, tokens[3]);
+		receiver.Execute({ AstInsType::Field, (vint32_t)CalculatorFields::Binary_right }, tokens[3]);
+		receiver.Execute({ AstInsType::EnumItem,(vint32_t)BinaryOp::Add }, tokens[3]);
+		receiver.Execute({ AstInsType::Field, (vint32_t)CalculatorFields::Binary_op }, tokens[3]);
+		receiver.Execute({ AstInsType::EndObject }, tokens[3]);
+		receiver.Execute({ AstInsType::Field, (vint32_t)CalculatorFields::Module_exported }, tokens[3]);
+		receiver.Execute({ AstInsType::EndObject }, tokens[3]);
+		auto node = receiver.Finished();
+		auto ast = node.Cast<Module>();
+		TEST_ASSERT(ast);
+		AssertAst(ast, LR"({
+    "$ast": "Module",
+    "exported": {
+        "$ast": "Binary",
+        "expanded": null,
+        "left": {
+            "$ast": "NumExpr",
+            "value": "1"
+        },
+        "op": "Add",
+        "right": {
+            "$ast": "NumExpr",
+            "value": "2"
+        }
+    },
+    "imports": []
+})");
+	});
+
 	TEST_CASE(L"import ... export ...")
 	{
 		WString input = LR"(
