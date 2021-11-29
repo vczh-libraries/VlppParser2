f4aae904b72628394b4a8148de3f3ee9184bf077
vczh <vczh@outlook.com>
2021-11-28 21:36:29 -0800

    ...

--- Test/UnitTest/ParserTest_ParserGen_Compiler/TestParserGen.cpp
+++ Test/UnitTest/ParserTest_ParserGen_Compiler/TestParserGen.cpp
@@ -1,5 +1,7 @@
 #include "../../../Source/ParserGen_Generated/ParserGenTypeAst_Json.h"
+#include "../../../Source/ParserGen_Generated/ParserGenRuleAst_Json.h"
 #include "../../../Source/ParserGen_Generated/ParserGenTypeParser.h"
+#include "../../../Source/ParserGen_Generated/ParserGenRuleParser.h"
 #include "../../Source/LogAutomaton.h"
 
 using namespace vl::glr::parsergen;
@@ -10,6 +12,7 @@ extern FilePath GetOutputDir(const WString& parserName);
 TEST_FILE
 {
 	TypeParser typeParser;
+	RuleParser ruleParser;
 
 	TEST_CATEGORY(L"Test ParserGen on Calculator")
 	{
@@ -18,10 +21,18 @@ TEST_FILE
 
 		TEST_CASE(L"Ast.txt")
 		{
-			auto astCode = File(dirParser / L"Syntax/Ast.txt").ReadAllTextByBom();
-			auto ast = typeParser.ParseFile(astCode);
+			auto input = File(dirParser / L"Syntax/Ast.txt").ReadAllTextByBom();
+			auto ast = typeParser.ParseFile(input);
 			auto actualJson = PrintAstJson<json_visitor::TypeAstVisitor>(ast);
 			File(dirOutput / L"Ast[Calculator].txt").WriteAllText(actualJson, BomEncoder::Utf8);
+		});
+
+		TEST_CASE(L"Syntax.txt")
+		{
+			auto input = File(dirParser / L"Syntax/Syntax.txt").ReadAllTextByBom();
+			auto ast = ruleParser.ParseFile(input);
+			auto actualJson = PrintAstJson<json_visitor::RuleAstVisitor>(ast);
+			File(dirOutput / L"Syntax[Calculator].txt").WriteAllText(actualJson, BomEncoder::Utf8);
 		});
 	});
 
