3663836654fc9a3daeb293a0bdca3f66fa1b7692
vczh <vczh@outlook.com>
2021-12-01 22:04:11 -0800

    UseRuleAppearAfter(Field|PartialRule)/FieldAssignedMoreThanOnce

--- Source/ParserGen/CompileSyntax_ValidateStructure.cpp
+++ Source/ParserGen/CompileSyntax_ValidateStructure.cpp
@@ -221,11 +221,107 @@ ValidateStructureRelationshipVisitor
 				, public virtual GlrSyntax::IVisitor
 				, public virtual GlrClause::IVisitor
 			{
+				struct Link
+				{
+					GlrRefSyntax*			ref = nullptr;
+					Link*					prev = nullptr;
+					Link*					next = nullptr;
+
+					Link(GlrRefSyntax* _ref) : ref(_ref) {}
+				};
+
+				struct LinkPair
+				{
+					Link*					first = nullptr;
+					Link*					last = nullptr;
+
+					void EnsureComplete()
+					{
+						CHECK_ERROR(!first || !first->prev, L"Illegal Operation!");
+						CHECK_ERROR(!last || !last->next, L"Illegal Operation!");
+					}
+
+					LinkPair Append(Link* link)
+					{
+						EnsureComplete();
+						if (first)
+						{
+							link->prev = last;
+							last->next = link;
+							return { first,link };
+						}
+						else
+						{
+							return { link,link };
+						}
+					}
+
+					LinkPair Connect(LinkPair pair)
+					{
+						EnsureComplete();
+						pair.EnsureComplete();
+						if (!first && !pair.first)
+						{
+							return {};
+						}
+						else if (!first)
+						{
+							return pair;
+						}
+						else if (!pair.first)
+						{
+							return *this;
+						}
+						else
+						{
+							last->next = pair.first;
+							pair.first->prev = last;
+							return { first,pair.last };
+						}
+					}
+
+					void CutAfter(Link* link, LinkPair& l1, LinkPair& l2)
+					{
+						EnsureComplete();
+						if (!first)
+						{
+							CHECK_ERROR(!link, L"Illegal Operation!");
+							l1 = {};
+							l2 = {};
+						}
+						else if (!link)
+						{
+							l2 = *this;
+							l1 = {};
+						}
+						else if (link == last)
+						{
+							l1 = *this;
+							l2 = {};
+						}
+						else
+						{
+							auto a = first;
+							auto b = last;
+							l1 = { a,link };
+							l2 = { link->next,b };
+							l1.last->next = nullptr;
+							l2.first->prev = nullptr;
+						}
+					}
+
+					operator bool() const
+					{
+						return last;
+					}
+				};
 			protected:
 				VisitorContext&				context;
 				RuleSymbol*					ruleSymbol;
 				GlrClause*					clause = nullptr;
 
+				LinkPair					existingFields;
+				LinkPair					existingPartials;
 			public:
 				ValidateStructureRelationshipVisitor(
 					VisitorContext& _context,
@@ -236,8 +332,44 @@ ValidateStructureRelationshipVisitor
 				{
 				}
 
+				~ValidateStructureRelationshipVisitor()
+				{
+					{
+						auto c = existingFields.first;
+						while (c)
+						{
+							auto n = c->next;
+							delete c;
+							c = n;
+						}
+					}
+					{
+						auto c = existingPartials.first;
+						while (c)
+						{
+							auto n = c->next;
+							delete c;
+							c = n;
+						}
+					}
+				}
+
 				void Visit(GlrRefSyntax* node) override
 				{
+					if (node->field)
+					{
+						existingFields = existingFields.Append(new Link(node));
+					}
+
+					vint ruleIndex = context.syntaxManager.Rules().Keys().IndexOf(node->name.value);
+					if (ruleIndex != -1)
+					{
+						auto fieldRule = context.syntaxManager.Rules().Values()[ruleIndex];
+						if (fieldRule->isPartial)
+						{
+							existingPartials= existingPartials.Append(new Link(node));
+						}
+					}
 				}
 
 				void Visit(GlrLiteralSyntax* node) override
@@ -246,6 +378,24 @@ ValidateStructureRelationshipVisitor
 
 				void Visit(GlrUseSyntax* node) override
 				{
+					if (existingFields)
+					{
+						context.global.AddError(
+							ParserErrorType::UseRuleAppearAfterField,
+							ruleSymbol->Name(),
+							node->name.value,
+							existingFields.last->ref->field.value
+							);
+					}
+					else if (existingPartials)
+					{
+						context.global.AddError(
+							ParserErrorType::UseRuleAppearAfterPartialRule,
+							ruleSymbol->Name(),
+							node->name.value,
+							existingPartials.last->ref->name.value
+							);
+					}
 				}
 
 				void Visit(GlrLoopSyntax* node) override
@@ -270,26 +420,73 @@ ValidateStructureRelationshipVisitor
 
 				void Visit(GlrAlternativeSyntax* node) override
 				{
+					auto currentFields = existingFields;
+					auto currentPartials = existingPartials;
+
 					node->first->Accept(this);
+
+					LinkPair firstFields, firstPartials;
+					existingFields.CutAfter(currentFields.last, existingFields, firstFields);
+					existingPartials.CutAfter(currentPartials.last, existingPartials, firstPartials);
+
 					node->second->Accept(this);
+
+					existingFields = existingFields.Connect(firstFields);
+					existingPartials = existingPartials.Connect(firstPartials);
+				}
+
+				void CheckAfterClause()
+				{
+					Dictionary<GlrRefSyntax*, vint> counters;
+					auto c = existingFields.first;
+					while (c)
+					{
+						vint index = counters.Keys().IndexOf(c->ref);
+						if (index == -1)
+						{
+							counters.Add(c->ref, 1);
+						}
+						else
+						{
+							counters.Set(c->ref, counters.Values()[index] + 1);
+						}
+					}
+
+					auto clauseType = context.clauseTypes[clause];
+					for (auto [key, value] : counters)
+					{
+						auto prop = clauseType->Props()[key->field.value];
+						if (prop->propType != AstPropType::Array && value > 1)
+						{
+							context.global.AddError(
+								ParserErrorType::FieldAssignedMoreThanOnce,
+								ruleSymbol->Name(),
+								clauseType->Name(),
+								prop->Name()
+							);
+						}
+					}
 				}
 
 				void Visit(GlrCreateClause* node) override
 				{
 					clause = node;
 					node->syntax->Accept(this);
+					CheckAfterClause();
 				}
 
 				void Visit(GlrPartialClause* node) override
 				{
 					clause = node;
 					node->syntax->Accept(this);
+					CheckAfterClause();
 				}
 
 				void Visit(GlrReuseClause* node) override
 				{
 					clause = node;
 					node->syntax->Accept(this);
+					CheckAfterClause();
 				}
 			};
 
