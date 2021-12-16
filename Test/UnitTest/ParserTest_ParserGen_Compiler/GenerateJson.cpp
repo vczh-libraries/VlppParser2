b8de1fc225a1ef8b6cdef89759459fd548c543f4
vczh <vczh@outlook.com>
2021-12-16 14:40:09 -0800

    ...

---
+++ Test/UnitTest/ParserTest_ParserGen_Compiler/GenerateJson.cpp
@@ -0,0 +1,15 @@
+#include "../../../Source/ParserGen_Generated/ParserGenTypeAst_Json.h"
+#include "../../../Source/ParserGen_Generated/ParserGenRuleAst_Json.h"
+#include "../../../Source/ParserGen_Generated/ParserGenTypeParser.h"
+#include "../../../Source/ParserGen_Generated/ParserGenRuleParser.h"
+#include "../../../Source/ParserGen/Compiler.h"
+#include "../../../Source/Ast/AstCppGen.h"
+#include "../../../Source/Lexer/LexerCppGen.h"
+#include "../../../Source/Syntax/SyntaxCppGen.h"
+#include "../../Source/LogAutomaton.h"
+
+using namespace vl::glr::parsergen;
+
+TEST_FILE
+{
+}
