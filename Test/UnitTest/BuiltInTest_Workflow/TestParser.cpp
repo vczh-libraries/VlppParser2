f71ed4b20863f49c0158f2d0b9db830f1ff247b5
vczh <vczh@outlook.com>
2021-12-16 23:04:16 -0800

    ...

--- Test/UnitTest/BuiltInTest_Workflow/TestParser.cpp
+++ Test/UnitTest/BuiltInTest_Workflow/TestParser.cpp
@@ -1,4 +1,8 @@
+#include "../../Source/BuiltIn-Workflow/Generated/WorkflowParser.h"
+#include "../../Source/BuiltIn-Workflow/Generated/WorkflowAst_Json.h"
 #include "../../Source/LogTrace.h"
+
+using namespace vl::glr::workflow;
 
 extern WString GetSourcePath();
 extern FilePath GetOutputDir(const WString& parserName);
@@ -6,8 +10,12 @@ extern FilePath GetOutputDir(const WString& parserName);
 TEST_FILE
 {
 	FilePath dirWorkflow = FilePath(GetSourcePath()) / L"../../Workflow/Test/Resources";
+	FilePath dirOutput = GetOutputDir(L"BuiltIn-Workflow");
+
 	List<File> indexFiles;
 	Folder(dirWorkflow).GetFiles(indexFiles);
+
+	workflow::Parser parser;
 
 	for (auto indexFile : indexFiles)
 	{
@@ -30,6 +38,31 @@ TEST_FILE
 
 				TEST_CASE(caseName)
 				{
+					WString inputCode = File(dirWorkflow / indexName / (caseName + L".txt")).ReadAllTextByBom();
+					if (indexName == L"Declaration")
+					{
+						auto ast = parser.Parse_Declaration(inputCode);
+						auto astJson = PrintAstJson<json_visitor::AstVisitor>(ast);
+						File(dirOutput / (L"Output[" + indexName + L"_" + caseName + L"].json")).WriteAllText(astJson, true, BomEncoder::Utf8);
+					}
+					else if (indexName == L"Expression")
+					{
+						auto ast = parser.Parse_Expression(inputCode);
+						auto astJson = PrintAstJson<json_visitor::AstVisitor>(ast);
+						File(dirOutput / (L"Output[" + indexName + L"_" + caseName + L"].json")).WriteAllText(astJson, true, BomEncoder::Utf8);
+					}
+					else if (indexName == L"Statement")
+					{
+						auto ast = parser.Parse_Statement(inputCode);
+						auto astJson = PrintAstJson<json_visitor::AstVisitor>(ast);
+						File(dirOutput / (L"Output[" + indexName + L"_" + caseName + L"].json")).WriteAllText(astJson, true, BomEncoder::Utf8);
+					}
+					else
+					{
+						auto ast = parser.Parse_Module(inputCode);
+						auto astJson = PrintAstJson<json_visitor::AstVisitor>(ast);
+						File(dirOutput / (L"Output[" + indexName + L"_" + caseName + L"].json")).WriteAllText(astJson, true, BomEncoder::Utf8);
+					}
 				});
 			}
 		});
