193ce22643a1a09dc1aed3af6b65cf62a622901f
vczh <vczh@outlook.com>
2021-12-24 19:44:14 -0800

    ...

--- Tools/GlrParserGen/GlrParserGen/Main.cpp
+++ Tools/GlrParserGen/GlrParserGen/Main.cpp
@@ -19,6 +19,22 @@ using namespace vl::glr::xml;
 		return 1;\
 	} while(false)
 
+#define EXIT_IF_PARSER_FAIL(ERRORS, TITLE)\
+	do\
+	{\
+		if (ERRORS.Count() > 0)\
+		{\
+			Console::SetColor(true, false, false, true);\
+			Console::WriteLine(TITLE);\
+			for (auto error : ERRORS)\
+			{\
+				Console::WriteLine(L"[row:" + itow(error.codeRange.start.row + 1) + L"][column:" + itow(error.codeRange.start.column + 1) + L"]: " + error.message);\
+			}\
+			Console::SetColor(true, true, true, false);\
+			return 1;\
+		}\
+	} while(false)
+
 #define READ_ATTRIBUTE(STORAGE, ELEMENT, NAME, PATH)\
 	do\
 	{\
@@ -89,41 +105,87 @@ int main(int argc, char* argv[])
 		InstallDefaultErrorMessageGenerator(parser, errors);
 		auto text = File(atow(argv[1])).ReadAllTextByBom();
 		config = XmlParseDocument(text, parser);
-
-		if (errors.Count() > 0)
-		{
-			Console::SetColor(true, false, false, true);
-			Console::WriteLine(L"Failed to read the input XML file.");
-			for (auto error : errors)
-			{
-				Console::WriteLine(L"[row:" + itow(error.codeRange.start.row + 1) + L"][column:" + itow(error.codeRange.start.column + 1) + L"]: " + error.message);
-			}
-			Console::SetColor(true, true, true, false);
-			return 1;
-		}
+		EXIT_IF_PARSER_FAIL(errors, L"Failed to read the input XML file.");
 	}
 
 	if (config->rootElement->name.value != L"Parser") EXIT_ERROR(L"Missing /Parser.");
 
-	Ptr<ParserSymbolManager> global;
-	Ptr<LexerSymbolManager>	lexerManager;
-	Ptr<SyntaxSymbolManager> syntaxManager;
-	List<Ptr<AstSymbolManager>> astManagers;
+	ParserSymbolManager global;
+	AstSymbolManager astManager(global);
+	LexerSymbolManager	lexerManager(global);
+	SyntaxSymbolManager syntaxManager(global);
 	FilePath generatedDir;
+	Dictionary<WString, WString> files;
 
 	Regex regexNamespace(L"^(<item>[^:]+)(::(<item>[^:]+))*$");
 	Regex regexIncludes(L"^(<item>[^;]+)(;(<item>[^;]+))*$");
 	auto indexItem = regexNamespace.CaptureNames().IndexOf(L"item");
 
-	global = MakePtr<ParserSymbolManager>();
-	READ_ATTRIBUTE(global->name, config->rootElement, L"name", L"/Parser@name");
-	READ_ELEMENT_ITEMS(global->includes, regexIncludes, config->rootElement, L"Includes", L"/Parser/Includes");
-	READ_ELEMENT_ITEMS(global->cppNss, regexNamespace, config->rootElement, L"CppNamespace", L"/Parser/CppNamespace");
-	READ_ELEMENT(global->headerGuard, config->rootElement, L"HeaderGuard", L"/Parser/HeaderGuard");
+	READ_ATTRIBUTE(global.name, config->rootElement, L"name", L"/Parser@name");
+	READ_ELEMENT_ITEMS(global.includes, regexIncludes, config->rootElement, L"Includes", L"/Parser/Includes");
+	READ_ELEMENT_ITEMS(global.cppNss, regexNamespace, config->rootElement, L"CppNamespace", L"/Parser/CppNamespace");
+	READ_ELEMENT(global.headerGuard, config->rootElement, L"HeaderGuard", L"/Parser/HeaderGuard");
 	{
 		WString outputDir;
 		READ_ELEMENT(outputDir, config->rootElement, L"OutputDir", L"/Parser/OutputDir");
 		generatedDir = workingDir / outputDir;
+	}
+
+	auto output = GenerateParserFileNames(global);
+	GenerateAstFileNames(astManager, output);
+	GenerateSyntaxFileNames(syntaxManager, output);
+
+	TypeParser typeParser;
+	RuleParser ruleParser;
+	List<ParsingError> errors;
+	InstallDefaultErrorMessageGenerator(typeParser, errors);
+	InstallDefaultErrorMessageGenerator(ruleParser, errors);
+
+	if (auto elementAsts = XmlGetElement(config->rootElement, L"Asts"))
+	{
+		List<Ptr<XmlElement>> asts;
+		CopyFrom(asts, XmlGetElements(elementAsts, L"Ast"));
+		if (asts.Count() == 0) EXIT_ERROR(L"Missing /Parser/Asts/Ast");
+
+		for (auto elementAst : asts)
+		{
+			WString name, file;
+			READ_ATTRIBUTE(name, elementAst, L"name", L"/Parser/Asts/Ast@name");
+			READ_ATTRIBUTE(file, elementAst, L"file", L"/Parser/Asts/Ast@file[@name=\"" + name + L"\"]");
+			File astFile = workingDir/file;
+			if (!astFile.Exists()) EXIT_ERROR(L"Missing ast definition file: " + astFile.GetFilePath().GetFullPath());
+			auto astInput = astFile.ReadAllTextByBom();
+			auto ast = typeParser.ParseFile(astInput);
+			EXIT_IF_PARSER_FAIL(errors, L"Syntax errors found in file: " + astFile.GetFilePath().GetFullPath());
+
+			auto astDefFile = astManager.CreateFile(name);
+			READ_ELEMENT_ITEMS(astDefFile->cppNss, regexNamespace, elementAst, L"CppNamespace", L"/Parser/Asts/Ast@file[@name=\"" + name + L"\"]/CppNamespace");
+			READ_ELEMENT_ITEMS(astDefFile->refNss, regexNamespace, elementAst, L"ReflectionNamespace", L"/Parser/Asts/Ast@file[@name=\"" + name + L"\"]/ReflectionNamespace");
+			READ_ELEMENT(astDefFile->classPrefix, elementAst, L"ClassPrefix", L"/Parser/Asts/Ast@file[@name=\"" + name + L"\"]/ClassPrefix");
+		}
+		WriteAstFiles(astManager, output, files);
+	}
+	else
+	{
+		EXIT_ERROR(L"Missing /Parser/Asts");
+	}
+
+	if (auto elementLexer = XmlGetElement(config->rootElement, L"Lexer"))
+	{
+
+	}
+	else
+	{
+		EXIT_ERROR(L"Missing /Parser/Asts");
+	}
+
+	if (auto elementSyntax = XmlGetElement(config->rootElement, L"Syntax"))
+	{
+
+	}
+	else
+	{
+		EXIT_ERROR(L"Missing /Parser/Asts");
 	}
 
 	return 0;
