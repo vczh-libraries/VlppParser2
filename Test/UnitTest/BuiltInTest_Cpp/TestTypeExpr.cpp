dfa67e3b7e7fe62729a009afd824ed53d85e50a6
vczh <vczh@outlook.com>
2022-03-05 15:12:52 -0800

    ...

---
+++ Test/UnitTest/BuiltInTest_Cpp/TestTypeExpr.cpp
@@ -0,0 +1,148 @@
+#include "../../Source/BuiltIn-Cpp/Generated/CppParser.h"
+
+using namespace vl;
+using namespace vl::collections;
+using namespace vl::filesystem;
+using namespace vl::regex;
+using namespace cpp_parser;
+
+extern WString GetTestParserInputPath(const WString& parserName);
+
+template<typename T>
+Ptr<T> ParseTypeExpr(cpp_parser::Parser& parser, const WString& code)
+{
+	return {};
+}
+
+TEST_FILE
+{
+	cpp_parser::Parser parser;
+
+	TEST_CATEGORY(L"Identifier")
+	{
+		List<WString> lines;
+		File(
+			FilePath(GetTestParserInputPath(L"BuiltIn-Cpp"))
+			/ L"Input"
+			/ L"TypeOrExpr_Identifier.txt"
+		).ReadAllLinesByBom(lines);
+		for (auto&& line : lines)
+		{
+			TEST_CASE(line)
+			{
+			});
+		}
+	});
+
+	TEST_CATEGORY(L"PrimitiveExprs")
+	{
+		List<WString> lines;
+		File(
+			FilePath(GetTestParserInputPath(L"BuiltIn-Cpp"))
+			/ L"Input"
+			/ L"TypeOrExpr_PrimitiveExprs.txt"
+		).ReadAllLinesByBom(lines);
+		for (auto&& line : lines)
+		{
+			TEST_CASE(line)
+			{
+			});
+		}
+	});
+
+	TEST_CATEGORY(L"PrimitiveTypes")
+	{
+		List<WString> lines;
+		File(
+			FilePath(GetTestParserInputPath(L"BuiltIn-Cpp"))
+			/ L"Input"
+			/ L"TypeOrExpr_PrimitiveTypes.txt"
+		).ReadAllLinesByBom(lines);
+		for (auto&& line : lines)
+		{
+			TEST_CASE(line)
+			{
+			});
+		}
+	});
+
+	TEST_CATEGORY(L"Types")
+	{
+		List<WString> lines;
+		File(
+			FilePath(GetTestParserInputPath(L"BuiltIn-Cpp"))
+			/ L"Input"
+			/ L"TypeOrExpr_Types.txt"
+		).ReadAllLinesByBom(lines);
+		for (auto&& line : lines)
+		{
+			TEST_CASE(line)
+			{
+			});
+		}
+	});
+
+	TEST_CATEGORY(L"Name")
+	{
+		List<WString> lines;
+		File(
+			FilePath(GetTestParserInputPath(L"BuiltIn-Cpp"))
+			/ L"Input"
+			/ L"TypeOrExpr_Name.txt"
+		).ReadAllLinesByBom(lines);
+		for (auto&& line : lines)
+		{
+			TEST_CASE(line)
+			{
+			});
+		}
+	});
+
+	TEST_CATEGORY(L"Exprs")
+	{
+		List<WString> lines;
+		File(
+			FilePath(GetTestParserInputPath(L"BuiltIn-Cpp"))
+			/ L"Input"
+			/ L"TypeOrExpr_Exprs.txt"
+		).ReadAllLinesByBom(lines);
+		for (auto&& line : lines)
+		{
+			TEST_CASE(line)
+			{
+			});
+		}
+	});
+
+	TEST_CATEGORY(L"Declarators_Anonymous")
+	{
+		List<WString> lines;
+		File(
+			FilePath(GetTestParserInputPath(L"BuiltIn-Cpp"))
+			/ L"Input"
+			/ L"TypeOrExpr_Declarators_Anonymous.txt"
+		).ReadAllLinesByBom(lines);
+		for (auto&& line : lines)
+		{
+			TEST_CASE(line)
+			{
+			});
+		}
+	});
+
+	TEST_CATEGORY(L"LambdaExprs")
+	{
+		List<WString> lines;
+		File(
+			FilePath(GetTestParserInputPath(L"BuiltIn-Cpp"))
+			/ L"Input"
+			/ L"TypeOrExpr_LambdaExprs.txt"
+		).ReadAllLinesByBom(lines);
+		for (auto&& line : lines)
+		{
+			TEST_CASE(line)
+			{
+			});
+		}
+	});
+}
