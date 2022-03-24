416b60db1a26aedbef59dfe68d2cfb0b768153be
vczh <vczh@outlook.com>
2022-03-23 18:27:19 -0700

    Begin TypeOrExpr_Exprs.txt

--- Test/UnitTest/BuiltInTest_Cpp/TestTypeExpr.cpp
+++ Test/UnitTest/BuiltInTest_Cpp/TestTypeExpr.cpp
@@ -155,9 +155,10 @@ TEST_FILE
 		{
 			if (line != L"")
 			{
-				//TEST_CASE(line)
-				//{
-				//});
+				TEST_CASE(line)
+				{
+					ParseTypeExpr<CppExprOnly>(parser, line);
+				});
 			}
 		}
 	});
