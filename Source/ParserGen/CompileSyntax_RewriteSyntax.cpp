ea88102a77c778664a83eb78bfc01b8d7e06a51a
vczh <vczh@outlook.com>
2022-08-04 10:50:15 -0700

    CreateRewrittenRules/FixRuleTypes

--- Source/ParserGen/CompileSyntax_RewriteSyntax.cpp
+++ Source/ParserGen/CompileSyntax_RewriteSyntax.cpp
@@ -11,16 +11,17 @@ namespace vl
 
 			struct RewritingContext
 			{
-				List<RuleSymbol*>					pmRules;
-				Dictionary<WString, GlrRule*>		originRules;
-				Dictionary<WString, GlrRule*>		rewrittenRules;
+				List<RuleSymbol*>						pmRules;
+				Dictionary<RuleSymbol*, GlrRule*>		originRules;
+				Dictionary<RuleSymbol*, GlrRule*>		lriRules;
+				Dictionary<RuleSymbol*, GlrRule*>		fixedAstRules;
 			};
 
 /***********************************************************************
 Calculating
 ***********************************************************************/
 
-			void CollectRewritingTargets(VisitorContext& vContext, RewritingContext& rContext, Ptr<GlrSyntaxFile> rewritten)
+			void CollectRewritingTargets(const VisitorContext& vContext, RewritingContext& rContext, Ptr<GlrSyntaxFile> rewritten)
 			{
 				for (auto rule : rewritten->rules)
 				{
@@ -36,6 +37,55 @@ Calculating
 Rules
 ***********************************************************************/
 
+			void CreateRewrittenRules(const VisitorContext& vContext, RewritingContext& rContext, Ptr<GlrSyntaxFile> rewritten)
+			{
+				for (auto ruleSymbol : rContext.pmRules)
+				{
+					rContext.originRules.Add(ruleSymbol, vContext.astRules[ruleSymbol]);
+				}
+
+				for (auto [ruleSymbol, origin] : rContext.originRules)
+				{
+					auto lri = MakePtr<GlrRule>();
+					rewritten->rules.Add(lri);
+					rContext.lriRules.Add(ruleSymbol, lri.Obj());
+
+					lri->name.value = origin->name.value;
+					origin->name.value += L"_LRI_Origin";
+				}
+			}
+
+			void FixRuleTypes(const VisitorContext& vContext, RewritingContext& rContext, SyntaxSymbolManager& syntaxManager, Ptr<GlrSyntaxFile> rewritten)
+			{
+				CopyFrom(rContext.fixedAstRules, vContext.astRules);
+				for (auto ruleSymbol : rContext.pmRules)
+				{
+					auto originRule = rContext.originRules[ruleSymbol];
+					auto lriRule = rContext.lriRules[ruleSymbol];
+					auto originSymbol = syntaxManager.CreateRule(originRule->name.value, originRule->codeRange);
+
+					originSymbol->ruleType = ruleSymbol->ruleType;
+					rContext.fixedAstRules.Set(originSymbol, originRule);
+					rContext.fixedAstRules.Set(ruleSymbol, lriRule);
+				}
+
+				for (auto [ruleSymbol, rule] : rContext.fixedAstRules)
+				{
+					if (!rule->type)
+					{
+						rule->type.value = ruleSymbol->ruleType->Name();
+					}
+				}
+
+				for (auto ruleSymbol : syntaxManager.Rules().Values())
+				{
+					ruleSymbol->isPartial = false;
+					ruleSymbol->assignedNonArrayField = false;
+					ruleSymbol->ruleType = nullptr;
+					ruleSymbol->lrFlags.Clear();
+				}
+			}
+
 /***********************************************************************
 Clauses
 ***********************************************************************/
@@ -44,7 +94,7 @@ Clauses
 RewriteSyntax
 ***********************************************************************/
 
-			Ptr<GlrSyntaxFile> RewriteSyntax(VisitorContext& context, SyntaxSymbolManager& syntaxManager, collections::List<Ptr<GlrSyntaxFile>>& files)
+			Ptr<GlrSyntaxFile> RewriteSyntax(const VisitorContext& context, SyntaxSymbolManager& syntaxManager, collections::List<Ptr<GlrSyntaxFile>>& files)
 			{
 				// merge files to single syntax file
 
@@ -60,8 +110,18 @@ RewriteSyntax
 				CollectRewritingTargets(context, rewritingContext, rewritten);
 
 				// create rewritten rules
+				CreateRewrittenRules(context, rewritingContext, rewritten);
 
-				// fix rule types
+				// fix rule types (fix syntaxManager.rules, clear RuleSymbol fields)
+				FixRuleTypes(context, rewritingContext, syntaxManager, rewritten);
+
+				// create left_recursion_inject clauses in rewritten rules
+
+				// convert prefix_merge clauses to left_recursion_placeholder clauses (fix syntaxManager.lrpFlags)
+
+				// rename rule references in normal rules
+
+				// rename rule references in origin rules
 
 				return rewritten;
 			}
