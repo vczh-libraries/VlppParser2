5d0319b001c5469c5e9bf43f8cff1558e2ed439b
vczh <vczh@outlook.com>
2022-08-23 09:00:00 -0700

    New test case: PrefixMerge2_Pm2 for throw and comma

--- Test/UnitTest/ParserTest_ParserGen_Compiler/TestParserGen.cpp
+++ Test/UnitTest/ParserTest_ParserGen_Compiler/TestParserGen.cpp
@@ -42,6 +42,7 @@ TEST_FILE
 	parsersToLoad.Add(ParserDef{ L"PrefixMerge3_LriNested",		L"TypeOrExpr",	L"Module" });
 	parsersToLoad.Add(ParserDef{ L"PrefixMerge4_LriMultiple",	L"TypeOrExpr",	L"Module" });
 	parsersToLoad.Add(ParserDef{ L"PrefixMerge5_Pm",			L"TypeOrExpr",	L"Module" });
+	parsersToLoad.Add(ParserDef{ L"PrefixMerge6_Pm2",			L"TypeOrExpr",	L"Module" });
 
 	for (auto&& [_parserName, _astName, _ruleName] : parsersToLoad)
 	{
