a802ed6c851138b4401c43425c9a498d1e00f1fd
vczh <vczh@outlook.com>
2022-08-26 17:08:07 -0700

    ...

--- Source/TraceManager/TraceManager_Input.cpp
+++ Source/TraceManager/TraceManager_Input.cpp
@@ -101,8 +101,9 @@ EndOfInput
 				for (vint32_t traceIndex = 0; traceIndex < traceCount; traceIndex++)
 				{
 					auto trace = concurrentTraces->Get(traceIndex);
-					auto& stateDesc = executable.states[trace->state];
-					if (trace->returnStack == -1 && stateDesc.endingState)
+					auto actualTrace = trace->state == -1 ? GetTrace(trace->predecessors.first) : trace;
+					auto& stateDesc = executable.states[actualTrace->state];
+					if (actualTrace->returnStack == -1 && stateDesc.endingState)
 					{
 						AddTrace(trace);
 					}
