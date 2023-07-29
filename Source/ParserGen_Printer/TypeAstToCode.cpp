c32c14c60914854b14da215f6a570a3b51c21c5b
vczh <vczh@outlook.com>
2023-07-29 01:38:12 -0700

    ...

--- Source/ParserGen_Printer/TypeAstToCode.cpp
+++ Source/ParserGen_Printer/TypeAstToCode.cpp
@@ -34,10 +34,63 @@ namespace vl::glr::parsergen
 
 			void Visit(GlrEnum* node) override
 			{
+				if (node->attPublic)
+				{
+					writer.WriteString(L"@public ");
+				}
+				writer.WriteString(L"enum ");
+				writer.WriteString(node->name.value);
+				writer.WriteLine(L"");
+				writer.WriteLine(L"{");
+				for (auto item : node->items)
+				{
+					writer.WriteString(L"  ");
+					writer.WriteString(item->name.value);
+					writer.WriteLine(L",");
+				}
+				writer.WriteLine(L"}");
 			}
 
 			void Visit(GlrClass* node) override
 			{
+				if (node->attPublic)
+				{
+					writer.WriteString(L"@public ");
+				}
+				if (node->attAmbiguous)
+				{
+					writer.WriteString(L"@ambiguous ");
+				}
+				writer.WriteString(L"class ");
+				writer.WriteString(node->name.value);
+				if (node->baseClass)
+				{
+					writer.WriteString(L" : ");
+					writer.WriteString(node->baseClass.value);
+				}
+				writer.WriteLine(L"");
+				writer.WriteLine(L"{");
+				for (auto prop : node->props)
+				{
+					writer.WriteString(L"  var ");
+					writer.WriteString(prop->name.value);
+					writer.WriteString(L" : ");
+					switch (prop->propType)
+					{
+					case GlrPropType::Token:
+						writer.WriteString(L"token");
+						break;
+					case GlrPropType::Type:
+						writer.WriteString(prop->propTypeName.value);
+						break;
+					case GlrPropType::Array:
+						writer.WriteString(prop->propTypeName.value);
+						writer.WriteString(L"[]");
+						break;
+					}
+					writer.WriteLine(L";");
+				}
+				writer.WriteLine(L"}");
 			}
 		};
 	}
