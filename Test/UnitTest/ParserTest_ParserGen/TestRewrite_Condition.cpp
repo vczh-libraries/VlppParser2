97216a2906ab877790e438c5d8cf0a99e4b44619
vczh <vczh@outlook.com>
2023-08-19 00:18:03 -0700

    ...

--- Test/UnitTest/ParserTest_ParserGen/TestRewrite_Condition.cpp
+++ Test/UnitTest/ParserTest_ParserGen/TestRewrite_Condition.cpp
@@ -68,4 +68,48 @@ Exp0 ::= "a":id as IdNode;
 
 		TestRewrite(typeParser, ruleParser, astCode, lexerCode, syntaxCode, rewrittenCode);
 	});
+
+	TEST_CASE(L"Test in Push (and)")
+	{
+		const wchar_t* syntaxCode =
+LR"SYNTAX(
+switch s,t;
+Exp0 ::= !( s, t; ?(s&&t?: "a":id | s&&!t?: "b":id | !s&&t?: "c":id | !s&&!t?: "d":id)) as IdNode;
+Exp1 ::= !( s,!t; ?(s&&t?: "a":id | s&&!t?: "b":id | !s&&t?: "c":id | !s&&!t?: "d":id)) as IdNode;
+Exp2 ::= !(!s, t; ?(s&&t?: "a":id | s&&!t?: "b":id | !s&&t?: "c":id | !s&&!t?: "d":id)) as IdNode;
+Exp3 ::= !(!s,!t; ?(s&&t?: "a":id | s&&!t?: "b":id | !s&&t?: "c":id | !s&&!t?: "d":id)) as IdNode;
+)SYNTAX";
+	
+		const wchar_t* rewrittenCode =
+LR"SYNTAX(
+Exp0 ::= "a":id as IdNode;
+Exp1 ::= "b":id as IdNode;
+Exp2 ::= "c":id as IdNode;
+Exp3 ::= "d":id as IdNode;
+)SYNTAX";
+	
+		TestRewrite(typeParser, ruleParser, astCode, lexerCode, syntaxCode, rewrittenCode);
+	});
+
+	TEST_CASE(L"Test in Push (or)")
+	{
+		const wchar_t* syntaxCode =
+LR"SYNTAX(
+switch s,t;
+Exp0 ::= !( s, t; ?(s||t?: "a":id | s||!t?: "b":id | !s||t?: "c":id | !s||!t?: "d":id)) as IdNode;
+Exp1 ::= !( s,!t; ?(s||t?: "a":id | s||!t?: "b":id | !s||t?: "c":id | !s||!t?: "d":id)) as IdNode;
+Exp2 ::= !(!s, t; ?(s||t?: "a":id | s||!t?: "b":id | !s||t?: "c":id | !s||!t?: "d":id)) as IdNode;
+Exp3 ::= !(!s,!t; ?(s||t?: "a":id | s||!t?: "b":id | !s||t?: "c":id | !s||!t?: "d":id)) as IdNode;
+)SYNTAX";
+	
+		const wchar_t* rewrittenCode =
+LR"SYNTAX(
+Exp0 ::= "a":id | "b":id | "c":id as IdNode;
+Exp1 ::= "a":id | "b":id | "d":id as IdNode;
+Exp2 ::= "a":id | "c":id | "d":id as IdNode;
+Exp3 ::= "b":id | "c":id | "d":id as IdNode;
+)SYNTAX";
+	
+		TestRewrite(typeParser, ruleParser, astCode, lexerCode, syntaxCode, rewrittenCode);
+	});
 }
