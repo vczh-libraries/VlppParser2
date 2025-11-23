2a1bfae9781234463893458400f84b15047c2167
vczh <vczh@outlook.com>
2025-11-23 14:52:37 -0800

    ...

--- Test/UnitTest/ParserTest_LexerAndParser/TestCreateCalculatorSyntax.cpp
+++ Test/UnitTest/ParserTest_LexerAndParser/TestCreateCalculatorSyntax.cpp
@@ -1,6 +1,8 @@
 #include "../../../Source/Syntax/SyntaxCppGen.h"
 #include "../../../Source/Ast/AstSymbol.h"
+#include "../../Source/Calculator/Parser/Calculator_Assembler.h"
 #include "../../Source/Calculator/Parser/Calculator_Lexer.h"
+#include "../../Source/LogParser.h"
 
 using namespace vl;
 using namespace vl::collections;
@@ -11,6 +13,7 @@ using namespace vl::glr::automaton;
 using namespace calculator;
 
 extern WString GetTestParserInputPath(const WString& parserName);
+extern FilePath GetOutputDir(const WString& parserName);
 extern void WriteFilesIfChanged(FilePath outputDir, Dictionary<WString, WString>& files);
 extern void InitializeCalculatorParserSymbolManager(ParserSymbolManager& manager);
 extern void GenerateCalculatorAst(AstSymbolManager& manager);
@@ -18,6 +21,15 @@ extern void GenerateCalculatorSyntax(AstSymbolManager& ast, SyntaxSymbolManager&
 
 TEST_FILE
 {
+	auto typeName = [](vint32_t type) { return WString::Unmanaged(CalculatorTypeName((CalculatorClasses)type)); };
+	auto fieldName = [](vint32_t field) { return WString::Unmanaged(CalculatorFieldName((CalculatorFields)field)); };
+	auto tokenName = [](vint32_t token)
+	{
+		auto n = CalculatorTokenId((CalculatorTokens)token);
+		auto d = CalculatorTokenDisplayText((CalculatorTokens)token);
+		return d ? L"\"" + WString::Unmanaged(d) + L"\"" : WString::Unmanaged(n);
+	};
+
 	TEST_CASE(L"CreateCalculatorLexer")
 	{
 		ParserSymbolManager global;
@@ -30,12 +42,22 @@ TEST_FILE
 		GenerateCalculatorAst(astManager);
 		GenerateCalculatorSyntax(astManager, syntaxManager);
 		TEST_ASSERT(global.Errors().Count() == 0);
+		{
+			syntaxManager.BuildCompactNFA();
+			TEST_ASSERT(global.Errors().Count() == 0);
+			syntaxManager.BuildCrossReferencedNFA();
+			TEST_ASSERT(global.Errors().Count() == 0);
+			syntaxManager.BuildAutomaton(CalculatorTokenCount, executable, metadata);
 
-		syntaxManager.BuildCompactNFA();
-		TEST_ASSERT(global.Errors().Count() == 0);
-		syntaxManager.BuildCrossReferencedNFA();
-		TEST_ASSERT(global.Errors().Count() == 0);
-		syntaxManager.BuildAutomaton(CalculatorTokenCount, executable, metadata);
+			LogAutomatonWithPath(
+				GetOutputDir(L"ParserGen") / L"Automaton[ParserGen_Calculator].txt",
+				executable,
+				metadata,
+				typeName,
+				fieldName,
+				tokenName
+				);
+		}
 
 		auto output = GenerateParserFileNames(global);
 		GenerateSyntaxFileNames(syntaxManager, output);
