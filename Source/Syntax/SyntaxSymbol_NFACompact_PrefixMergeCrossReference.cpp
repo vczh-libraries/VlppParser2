5c099e278cb9e3bbcf0b3422d1264b4da45cac19
vczh <vczh@outlook.com>
2025-12-12 18:39:29 -0800

    Update SyntaxSymbol_NFACompact_PrefixMergeCrossReference.cpp

--- Source/Syntax/SyntaxSymbol_NFACompact_PrefixMergeCrossReference.cpp
+++ Source/Syntax/SyntaxSymbol_NFACompact_PrefixMergeCrossReference.cpp
@@ -802,28 +802,18 @@ SyntaxSymbolManager::PrefixMergeCrossReference_Apply
 				*   recursive cross-reference merges will not be implemented at the moment
 				*   treat it as a special case which only merge prefixes in the size of one rule
 				* 
-				* Pay attention to complex situation when prefix calls trees and only sub trees merge
-				* 
-				* This function checks all states with multiple outgoing Rule transitions
-				*   if any mergable transitions are found, all outgoing Rule transitions will have to be reworked
-				*   because if a sub tree merges with another
-				*   all other roots of sub trees in all ancestors will have to be connected from that state
-				*   e.g.
-				*              +- r2 ..
-				*              |      +- r4 ..
-				*     A -> r1 -+- r3 -+  r5 ..
+				* PrefixMergeCrossReference_Solve worked out how many transitions need to be reworked
+				*   e.g. (a start set graph, assuming left nodes are selected prefix rules)
+				*              +-> r2 ..
+				*              |       +-> r4 ..
+				*     A -> r1 -+-> r3 -+-> r5 ..
 				*              |
-				*              +- r6..
+				*              +-> r6..
 				*   when r4 merges, (r2, r5, r6) will need similar edges to start from A:
 				*     A -+- r4 (injects into r3)
 				*        +- r5 (injects into r3)
 				*        +- r2 (injects into r1)
 				*        +- r6 (injects into r1)
-				*   only first level transitions are unaffected, all extra injections include Token from (r1, r3)
-				* 
-				* Do not merge edges when it is equivalent to just call those rules.
-				*   It means merging should happen between different first level sub trees.
-				*   Be careful when merging destroy the prefix structure, do we need to keep a copy of unmerged rule?
 				*/
 			}
 		}
