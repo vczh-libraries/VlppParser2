d73cdfa1418f792c1b364743650faeae809860e8
vczh <vczh@outlook.com>
2025-12-17 20:39:06 -0800

    Passed Feature_Pwl

--- Test/Source/PrefixMerge5_Pm/Generated/PrefixMerge5_PmModuleParser.h
+++ Test/Source/PrefixMerge5_Pm/Generated/PrefixMerge5_PmModuleParser.h
@@ -21,11 +21,11 @@ namespace prefixmerge5_pm
 		_PrimitiveExprOnly = 14,
 		_LongType = 19,
 		_Expr0 = 32,
-		_Expr1 = 41,
-		_Expr = 49,
-		Module = 55,
-		ExprModule = 59,
-		TypeModule = 62,
+		_Expr1 = 42,
+		_Expr = 50,
+		Module = 56,
+		ExprModule = 62,
+		TypeModule = 65,
 	};
 
 	const wchar_t* ModuleParserRuleName(vl::vint index);
